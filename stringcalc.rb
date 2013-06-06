405507398f214d10dcb92541be93c1abbcc1f89a
Eric <eglassman@gmail.com>
2013-06-05 18:29:57 -0700

    iteration three of kata pt.2

--- stringcalc.rb
+++ stringcalc.rb
@@ -2,17 +2,28 @@ class Calculator
 
   def add(string)
     return 0 if string.empty?  
-    numbers = decompose(string).select { |n| n < 1000 }
+    numbers   = decompose(string)
     negatives = numbers.select { |n| n < 0 }
-    if negatives.empty?
+    
+   if negatives.empty?
       return numbers.reduce { |acc, n| acc + n }
     else 
       raise "negatives not allowed: #{negatives.join(", ")}" 
     end 
   end
 
+  private
+
   def decompose(input)
-    input.scan(/-?\d+,?/).map { |num| num.to_i }
+    max_threshold( extract_numbers( input ) )
+  end
+
+  def extract_numbers(input)
+    input.scan(/-?\d+,?/).map { |num| num.to_i } 
+  end
+
+  def max_threshold(input)
+    input.select { |n| n < 1000 }
   end
 
 end
